f960530b87277e526ed495c2846e7d3ea82960ce
Beta-23 <alfred.dominguez@gmail.com>
2019-12-04 22:02:08 -0800

    Created the card component for rolodex and changed the state to robots to match api img

--- src/App.js
+++ src/App.js
@@ -9,25 +9,21 @@ class App extends Component {
     super();
 
     this.state = {
-      companies: []
+      robots: []
     };
   }
   // Life Cycle Method because of the class component
     componentDidMount(){
       fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
-      .then(users => this.setState({ companies: users }));
+      .then(users => this.setState({ robots: users }));
     }
   
   render() {
     return (
       <div className='App'>
-      <CardList name= 'Al'>
-        {
-          // Only renders or changes the element that id that has been modify on the DOM v
-          // ia the map array method to create a new array
-          this.state.companies.map(company => (<h1 key= { company.id }>{ company.name }</h1>))
-        }
+      <CardList robots= {this.state.robots}>
+        
       </CardList>
         
       </div>
