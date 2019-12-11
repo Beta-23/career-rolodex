1272c1e7e5b96cad7288f42082fc0ae7eefafc9d
Beta-23 <alfred.dominguez@gmail.com>
2019-12-11 13:42:49 -0800

    Updates

---
+++ precache-manifest.a862f75ec32cb5b7d0d24651347f6245.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49e17c6db9ad4ad3340b19ca5b5ff696",
+    "url": "/career-rolodex/index.html"
+  },
+  {
+    "revision": "0d386bad86e3983c59de",
+    "url": "/career-rolodex/static/css/main.5e10442b.chunk.css"
+  },
+  {
+    "revision": "ce58c072908dd2fa0b9e",
+    "url": "/career-rolodex/static/js/2.c690ae41.chunk.js"
+  },
+  {
+    "revision": "0d386bad86e3983c59de",
+    "url": "/career-rolodex/static/js/main.b3352683.chunk.js"
+  },
+  {
+    "revision": "19b293d2e302b13b1e43",
+    "url": "/career-rolodex/static/js/runtime-main.324940a8.js"
+  }
+]);
